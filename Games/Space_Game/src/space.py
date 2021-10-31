02da123a2c94c98dc3472ec85b7ef773f5ed6bf9
Cedric <cedric.dve1@gmail.com>
2021-10-31 03:20:18 +0100

    Authentication

--- Games/Space_Game/src/space.py
+++ Games/Space_Game/src/space.py
@@ -418,7 +418,20 @@ def game_over_text():
 
 
 
-
+# Authentication 
+# -- Before the game starts a user need to authenticate
+print("Please login to be able to save you points: \n Press (x) to login  \n Press (y) to register \n ")
+# Login
+if keyboard.read_key() == "x":
+    print("Login")
+    login() 
+# Register 
+if keyboard.read_key() == "y":
+    print("Register")
+    register()
+    print("You are now ready to go !")
+    print("The game will start .. NOW")
+    
 # Game loop: game will run until 'run != true'
 run = True
 while run:
